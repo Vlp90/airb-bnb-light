10ccfc0ee22e7e58ed875ac87d40ac61bf4c7531
Vlp90 <vladimir.leloup@gmail.com>
2020-09-02 14:51:01 +0200

    footer done

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="app">
      <Header />
       <Home />
-      {/* header */}
-      {/* banner */}
+      <Footer />
+     
 
-      {/* Cards */}
 
       {/* Footers */}
 
