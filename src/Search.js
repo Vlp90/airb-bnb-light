02a33d19c244d89f3cb7e2a552a03304f2745e17
Vlp90 <vladimir.leloup@gmail.com>
2020-09-02 12:39:56 +0200

    install date fns and react router dom

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,13 @@
 // RFCE
 
-import React from 'react'
-import "./Search.css"
+import React, {useState} from 'react';
+import './Search.css';
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { Button } from "@material-ui/core";
+import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
 
 function Search() {
     return (
